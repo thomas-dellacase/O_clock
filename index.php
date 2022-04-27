795dbaeb3b83180959bc5eadafd2b8e676967bf5
thomas-dellacase <thomas.dellacase@laplateforme.io>
2022-04-27 08:28:33 +0200

    timer done

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="script.js"></script>
+    <script src="script.js" defer></script>
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body>
@@ -19,17 +20,30 @@
             <button id="stop">stop</button>
             <button id="reset">reset</button>
             <button id="tours">tours</button>
-            <h2>Tours</h2><br>
+            <h2>Tours</h2>
             <p id="lap"></p>
         </article>
-
+<!-- 
         <article id="timero">
             <p id='timer'></p>
             <button id="add">Ajouté</button>
             <button id="remove">Suprimé</button>            
             <button id="starting">start</button>
             <button id="remet">reset</button>
-        </article>
+        </article> -->
+
+        <div id="container">
+            <p id="hour-label" class="label">Heure</p>
+            <p id="minute-label" class="label">Minutes</p>
+            <p id="sec-label" class="label">Secondes</p>
+            <input id="hour" type="number" max="99" min="0" value="0" class="time">
+            <p id="p1" class="semicolon">:</p>
+            <input id="minute" type="number" max="60" min="0" value="0" class="time">
+            <p id="p2" class="semicolon">:</p>
+            <input id="sec" type="number" max="60" min="0" value="0" class="time">
+            <button id="starting" class="btn">Start</button>
+            <button id="reseting" class="btn">Reset</button>
+        </div>
     </main>
 </body>
 </html>
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-'use strict';
+
 document.addEventListener('DOMContentLoaded',()=>{
     let h1 = document.querySelector("#h1");
     let start = document.querySelector("#start");
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded',()=>{
     let timer = document.querySelector("#timer");
     let add = document.querySelector("#add");
     let remove = document.querySelector("#remove");
-    let starting = document.querySelector("#starting");
+    //let starting = document.querySelector("#starting");
     let remet = document.querySelector("#remet");
     let submit = document.querySelector("#submit");
     let sec = 0;
@@ -68,115 +68,104 @@ document.addEventListener('DOMContentLoaded',()=>{
 
     //--------------------------------------------------------------------------------------
 
+    // let startingMinutes = 10;
+    // let time = startingMinutes * 60;
 
-    // function minuteur() {
-    //     sec --;
-    //     if(sec <= 0){
-    //         sec == 59;
-    //         min --;
-    //     }
-    //     if(min <= 0){
-    //         min == 59;
-    //         hrs --;
-    //     }
-    //     if(hrs <= 0){
-    //         hrs == 0;
-    //     }
-        
-    //     let minuteur = hrs + ":" + min + ":" + sec;
-    //     setInterval(minuteur, 1000);
-    //     // console.log(minuteur);
-    //     timer.innerHTML = minuteur;
-    //     //timeout = setTimeout(minuteur,100);
-    // }
 
-    // function startingo(){
-    //     starting.addEventListener("click",minuteur);
+    // function updateCountDown() {
+    //     let minutes = Math.floor(time/60);
+    //     let seconds = time%60;
 
-    //     console.log(minuteur);
-    // }
-    // startingo();
+    //     seconds = seconds < 10 ? '0' + seconds : seconds;
+    //     timer.innerHTML = `${minutes}:${seconds}`;
+    //     time --;
 
-    // function remeta() {
-    //     sec = 0;
-    //     min = 0;
-    //     hrs = 0;
-    //     let minute = hrs + ":" + min + ":" + sec;
-    //     timer.innerHTML = minute;
-    // }
+    //     document.getElementById("starting").addEventListener("click",()=>{
+    //         setInterval(updateCountDown, 1000);
+    //         //return interval;
+    //     });
 
-    // function remetage (){
-    //     remet.addEventListener("click",remeta);
-    // }
-    // remetage();
-
-    // function addTime() {
-    //     sec ++;
-    //     let minute = hrs + ":" + min + ":" + sec;
-    //     timer.innerHTML = minute;
-    // }
-
-    // function addSeconds() {
-    //     add.addEventListener("click", addTime);
-    //     addTime();
-    // }
-    // addSeconds();
-
-    let startingMinutes = 10;
-    let time = startingMinutes * 60;
-
-    function startCount(){
-        document.getElementById("starting").addEventListener("click",()=>{
-            let interval = setInterval(updateCountDown, 1000);
-            return interval;
-        });
-    }
-    startCount();
-
-    // function resetCount(){
-    //     document.getElementById("remet").addEventListener("click",()=>{
-    //         let minutes = 10;
-    //         let seconds = 0;
-    //         clearInterval(interval);
-    //         timer.innerHTML = `${minutes}:${seconds}`;
-    //     })
-    // }
-    // resetCount();
-
-    function updateCountDown() {
-        let minutes = Math.floor(time/60);
-        let seconds = time%60;
-
-        seconds = seconds < 10 ? '0' + seconds : seconds;
-        timer.innerHTML = `${minutes}:${seconds}`;
-        time --;
-
-            if(minutes == 0 && seconds == 0){
-                clearInterval(interval);
-                window.alert("THE FINAL COUNT DOWN TU TUUUUUU TU TU TU TU");
-            }
+    //         if(minutes == 0 && seconds == 0){
+    //             clearInterval(interval);
+    //             window.alert("THE FINAL COUNT DOWN TU TUUUUUU TU TU TU TU");
+    //         }
             
+    //     }
 
-            document.getElementById("remet").addEventListener("click",()=>{
-                let minutes = 10;
-                let seconds = 0;
-                clearInterval(interval);
-                timer.innerHTML = `${minutes}:${seconds}`;
-            })
+    //     function resetCount(){
+    //         document.getElementById("remet").addEventListener("click",()=>{
+    //             clearInterval();
+    //             //var interval = setInterval(updateCountDown, 1000);
+    //             let minutes = 10;
+    //             let seconds = 0;
+    //             timer.innerHTML = `${minutes}:${seconds}`;
+    //         })
+    //     } resetCount();
+        
 
+    //     function addMin(){
+    //         updateCountDown()
+    //         document.getElementById("add").addEventListener("click",()=>{
+    //             minutes ++;
+    //         });
+    //     }
+    //     addMin(); 
+
+    var starting = document.getElementById("starting");
+    var reseting = document.getElementById("reseting");
+
+    var h = document.getElementById("hour");
+    var m = document.getElementById("minute");
+    var s = document.getElementById("sec");
+
+    var startTimer = null;
+
+    function timera(){
+        if(h.value == 0 && m.value == 0 && s.value == 0){
+            alertCount()
+            h.value = 0;
+            m.value = 10;
+            s.value = 0;
+            stopTimer()
+        }else if(s.value !=0){
+            s.value --;
+        }else if(m.value !=0 && s.value == 0){
+            s.value = 59;
+            m.value --;
+        }else if(h.value !=0 && m.value == 0){
+            m.value == 60;
+            h.value--;
         }
-        function addMin(){
-            updateCountDown()
-            document.getElementById("add").addEventListener("click",()=>{
-                minutes ++;
-            });
+        return
+        
+    }
+
+    function alertCount(){
+        if(h.value == 0 && m.value == 0 && s.value == 0){
+            window.alert("THE FINAL COUNT DOWN TU TUUUUUU TU TU TU TU");
+            return
         }
-        addMin(); 
+    }
+        
+    function stopTimer(){
+        clearInterval(startTimer);
+    }
 
-
-
-    
-    
+    starting.addEventListener("click",function(){
+        //console.log("hello");
+        function startInterval(){
+            startTimer = setInterval(function(){
+                timera();
+            }, 1000)
+        }
+        startInterval();
+    })
+    reseting.addEventListener("click", function(){
+        h.value = 0;
+        m.value = 10;
+        s.value = 0;
+        stopTimer(); 
+    })
 //--------------------------------------------------------------------------------------------------
     function showTimer(){
         const date = new Date()
@@ -189,31 +178,8 @@ document.addEventListener('DOMContentLoaded',()=>{
         setTimeout(showTimer, 1000);
     }
     showTimer();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
 })
+
+
+
+
